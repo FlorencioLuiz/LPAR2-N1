51d14a5e38082b3970cd36c486d0d4b769f173c9
Luiz Florencio <luizflorencio64@gmail.com>
2024-04-25 22:17:26 -0300

    corrigindo e finalizado

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 console.log('Olá!')
 
+import cors from 'cors'
 import express from 'express'
 import dotenv from 'dotenv'
 import dataBase from './database/ormconfig'
@@ -9,6 +10,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use(cors()) // habilita o CORS
 app.use(express.json()) // habilita o express para receber dados no formato json
 app.use(routes) // habilita as rotas
 
@@ -16,3 +18,7 @@ app.listen(port, () => {
   console.log(`Servidor executando na porta ${port}`)
   console.log(`Banco de dados`, dataBase.isInitialized ? 'inicializado' : 'não inicializado')
 })
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://meuapp.com']
+}))
